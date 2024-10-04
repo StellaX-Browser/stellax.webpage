5887ccf64a06f05320676d9e530081ef56d8bc4b
SRUTHISAMHITA28 <141323698+SRUTHISAMHITA28@users.noreply.github.com>
2024-10-04 19:43:51 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex gap-3 p-8">
-          Hello!!
+          <B>Hello!!</B>
         </div>
         <div>
           We are coming soon!
