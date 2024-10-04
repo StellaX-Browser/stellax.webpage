5fce5ef609685b0e1c80e7df7164935829ec3803
SRUTHISAMHITA28 <141323698+SRUTHISAMHITA28@users.noreply.github.com>
2024-10-04 20:28:08 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 //import Image from "next/image";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] mx-8">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex gap-3 p-8">
+        <div className="flex gap-3 p-8 ">
           <strong><em>Hello!!</em></strong>
         </div>
-        <div>
+        <div >
           We are coming soon!
         </div>
       </main>
