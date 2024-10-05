d27aa66cbb4d31b1ba73cce2f87f2296413255e7
VVS Basanth Pedapati <vvs.pedapati@hotmail.com>
2024-10-05 23:58:03 +0530

    test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           Hello!!
         </div>
         <div>
-          We are coming soon!
+          StellaX Code assistance extension pack will be released soon
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
