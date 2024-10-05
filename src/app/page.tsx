f1f74c45f0a148117be264123e91f993e2a35987
VVS Basanth Pedapati <vvs.pedapati@hotmail.com>
2024-10-06 00:11:16 +0530

    test3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
-          Hello!!
+          Hello Developers!!
         </div>
         <div>
-          StellaX Code assistance extension pack will be released soon
+          StellaX is going to release a cross-browser extension which will help you with understanding the errors.
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
