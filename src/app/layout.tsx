f54f57c67d4f532e772596e228e2a94280240ea5
SRUTHISAMHITA28 <141323698+SRUTHISAMHITA28@users.noreply.github.com>
2024-10-10 00:08:44 +0530

    footer updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Footer from "../components/footer"; // Import Footer component
 import "./globals.css";
 
 const geistSans = localFont({
@@ -25,10 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <Footer /> {/* Call the Footer component */}
       </body>
     </html>
   );
